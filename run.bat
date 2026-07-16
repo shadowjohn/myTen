f03f9390697a7c0039c5a33d44f1c31efcf78752
John <john@gis.tw>
2026-07-16 13:41:34 +0800

    Add Windows Cordova Android launcher

--- run.bat
+++ run.bat
@@ -1,1 +1,15 @@
-cordova run android
+@echo off
+setlocal
+cd /d "%~dp0"
+
+if not defined CORDOVA_JAVA_HOME set "CORDOVA_JAVA_HOME=%ProgramFiles%\Java\jdk-17"
+if not exist "%CORDOVA_JAVA_HOME%\bin\java.exe" (
+    echo JDK 17 not found: "%CORDOVA_JAVA_HOME%"
+    exit /b 1
+)
+
+set "JAVA_HOME=%CORDOVA_JAVA_HOME%"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
+call npx cordova run android %*
+exit /b %ERRORLEVEL%
